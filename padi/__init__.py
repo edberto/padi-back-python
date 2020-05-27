c0f79a5152862f55d3dbb159d592b740b0dd3737
edberto <45651625+edberto@users.noreply.github.com>
2020-05-27 12:14:23 +0700

    Implemented find condition info API (#15)
    * Established repository structure and added http listener
    
    * Implemented find condition info API
    
    * added prevention to return field

--- padi/__init__.py
+++ padi/__init__.py
@@ -1,24 +1,22 @@
 import os
 from flask import Flask
+from flask_pymongo import MongoClient
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KET='dev',
+)
+app.config.from_pyfile('config.py')
+
+mongo_client = MongoClient(app.config['MONGO_URI'])
+padi_db = mongo_client['padi']
+
+try:
+    os.makedirs(app.instance_path)
+except Exception as ex:
+    pass
 
 from .routes import *
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KET='dev',
-    )
+app.register_blueprint(routes)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-    
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
-    app.register_blueprint(routes)
-
-    return app
--- padi/routes/__init__.py
+++ padi/routes/__init__.py
@@ -2,6 +2,7 @@ import json
 import datetime
 
 from flask import Blueprint
+from bson.objectid import ObjectId
 
 routes = Blueprint('routes', __name__)
 
@@ -9,12 +10,15 @@ class JSON_Improved(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime.datetime):
             return str(o)
+        elif isinstance(o, ObjectId):
+            return str(o)
         
         return json.JSONEncoder.default(self, o)
 
 routes.json_encoder = JSON_Improved
 
 from .index import *
+from .condition import *
 
 
 
