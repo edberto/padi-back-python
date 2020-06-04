7623867b865c140af11eddfda2a48bea9b3f6a09
edberto <edbertongko88@gmail.com>
2020-06-04 16:43:08 +0700

    Reset repo due to design change

--- padi/routes/index.py
+++
@@ -1,7 +0,0 @@
-import datetime
-from . import routes
-from .wrapper import wrap
-
-@routes.route('/')
-def index():
-    return wrap('Hello World', 'Success', 200)                                                                                                                   
--- padi/routes/wrapper.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-
-def wrap(data, message, status):
-    return jsonify({
-        "data": data,
-        "message": message,
-        "status": status,
-    })
